73b588cd314f182ae67b20e2d5a66dc4500e58bc
Jeans-Patel <47866149+Jeans-Patel@users.noreply.github.com>
2019-03-12 23:50:07 -0400

    Completed - 2 functions

--- src/functions.c
+++ src/functions.c
@@ -61,7 +61,7 @@ void userInput(FILE * fp)
         else
         {
             /* If the user enters something then it calculates the address
-            of first name and last name */
+            of first name (current address) and last name (next address) */
             strcpy(fn, buffer);
             person->first_name_posn = ftell(fp) + sizeof(contact);
             length[0] = strlen(buffer) + 1;
@@ -75,6 +75,12 @@ void userInput(FILE * fp)
         ln = malloc(strlen(buffer)+1);
         if (strcmp(buffer, "\n") == 0)
         {
+            /* 
+            Logic is a bit different here
+            If they enter nothing I first calculate the position
+            of the next name using the calculations from the previous
+            fgets(). Then I set the pointer to 0
+            */
             person->company_name_posn = person->last_name_posn;
             person->last_name_posn = 0;
             ln[0] = '\0';
@@ -87,6 +93,7 @@ void userInput(FILE * fp)
             person->company_name_posn = person->last_name_posn + length[1];
         }
 
+        /* Exact same steps except with company name instead */
         printf("Company Name: ");
         fgets(buffer, 1002, stdin);
         strtok(buffer, "\n");
@@ -106,14 +113,18 @@ void userInput(FILE * fp)
             person->email_posn = person->company_name_posn + length[2];
         }
         
+        /* Reading phone number */
         do
         {
+            /* Recets the check */
             check1 = 0;
             printf("Phone Number (enter only number): ");
             fgets(buffer, 1002, stdin);
             strtok(buffer, "\n");
             for (i = 0; i < strlen(buffer); ++i)
             {
+                /* Checks if each character they entered is a number, if not
+                it will change the check to repeat the loop. */
                 if(isdigit(buffer[i]) == 0)
                 {
                     check1 = 1;
@@ -121,10 +132,12 @@ void userInput(FILE * fp)
             }
             if(check1 == 0)
             {
+                /* If they only entered numbers it sets the phone_number to their input */
                 person->phone_number = strtol(buffer, &pn, 10);
             }
         } while(check1 != 0);
 
+        /* Same logic as company name */
         printf("Email: ");
         fgets(buffer, 1002, stdin);
         strtok(buffer, "\n");
@@ -143,19 +156,23 @@ void userInput(FILE * fp)
             person->next = person->email_posn + length[3];
         }
 
+        /* Writes the addresses to the file */
         fwrite(person, sizeof(contact), 1, fp);
         
+        /* Writes the actual data to the file */
         fwrite(fn, length[0], 1, fp);
         fwrite(ln, length[1], 1, fp);
         fwrite(cn, length[2], 1, fp);
         fwrite(e, length[3], 1, fp);
 
+        /* Free dynamically allocated memory */
         free(fn);
         free(ln);
         free(cn);
         free(e);
         free(person);
 
+        /* Point them to NULL so they can be reused when it loops */
         fn = NULL;
         ln = NULL;
         cn = NULL;
@@ -167,64 +184,79 @@ void userInput(FILE * fp)
 
 int userRead(FILE * fp)
 {
+    /* Declaring variables */
     char buffer[1002];
     int check1 = 0;
     int returnvalue = 0;
     unsigned long phonenumber;
     contact * addresses;
 
+    /* Move to the beginning of the file to check all entries */
     fseek(fp, 0, SEEK_SET);
 
+    /* Asking the user */
     printf("Do you wish to retrieve a contact? (Yes or No)?: ");
     fgets(buffer, 5, stdin);
     
     if(strcmp(buffer, "No\n") != 0)
     {   
         returnvalue = 1;
+        /* Dynamically allocates memory */
         addresses = malloc(sizeof(contact));
 
+        /* Asks for the phone number */
         printf("Phone Number: ");
         fscanf(stdin, "%lu", &phonenumber);
         fgetc(stdin);
 
         while(1)
         {
+            /* Reads the addresses from the file and puts them in the struct */
             fread(addresses, sizeof(contact), 1, fp);
+
+            /* Stop the while loop if the phone number is found */
             if(addresses->phone_number == phonenumber)
             {
                 check1 = 0;
                 break;
             }
+            /* If the end of file is reached before finding the number then it breaks
+            the loop */
             else if(feof(fp))
             {
                 check1 = 1;
                 break;
             }
+            /* If the number isn't found or the eof isnt reached then it goes to
+            the next set of addresses */
             else
             {
                 fseek(fp, addresses->next, SEEK_SET);   
             }
         }
 
+        /* If the end of file is reached it prints the message, else it prints the information */
         if (check1 == 1)
         {
             printf("No match found\n");
         }
         else
         {
-            
+            /* If the address points to 0 it doesnt print any information */
             if(addresses->first_name_posn == 0)
             {
                 printf("First Name:\n");
             }
             else
             {
+                /* If the address is not 0 then it moves to that position in the file
+                and reads it */
                 fseek(fp, addresses->first_name_posn, SEEK_SET);
                 fread(buffer, addresses->last_name_posn - addresses->first_name_posn, 1, fp);
                 printf("First Name: %s\n", buffer);
             }
 
-            
+            /* Same logic as first name */
             if(addresses->last_name_posn == 0)
             {
                 printf("Last Name:\n");
@@ -236,6 +268,7 @@ int userRead(FILE * fp)
                 printf("Last Name: %s\n", buffer);
             }
             
+            /* Same logic as first name */
             if(addresses->company_name_posn == 0)
             {
                 printf("Company Name:\n");
@@ -247,8 +280,10 @@ int userRead(FILE * fp)
                 printf("Company Name: %s\n", buffer);
             }
 
+            /* Prints the number they wanted to find */
             printf("Phone Number: %lu\n", addresses->phone_number);
 
+            /* Same logic as first name */
             if(addresses->email_posn == 0)
             {
                 printf("Email:\n");
@@ -261,9 +296,10 @@ int userRead(FILE * fp)
             }
 
         }
-
+        /*Free dynamically allocated struct*/
         free(addresses);  
     }
-
+    /* Returns 1 if they answered Yes, 0 if they answerd No 
+    (further explained in main)*/
     return(returnvalue);
 }
--- src/main.c
+++ src/main.c
@@ -2,21 +2,29 @@
 
 int main(int argc, char ** argv)
 {
+    /* Declaring variable */
     int check = 1;
+    
+    /* Opens the file if it exists or makes it if it doesn't */
     FILE * fp = fopen("myContactList.db", "ab+");
 
+    /* If there's an error it prints and error message and closes the program */
     if (fp == NULL)
     {
         printf("There was an error with the file,\n");
         exit(1);
     }
 
+    /* Keeps asking the user if they want to make a new contact
+    or search for a contact until the say No to both */
     while (check != 0)
     {
         userInput(fp);
         check = userRead(fp);
     }
     
+    /* Closes the file */
     fclose(fp);
+    
     return 0;
 }
